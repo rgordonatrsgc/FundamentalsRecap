25ba05ddb4bb6dda87a8b6ca2f1c98fe66bd7403
Russell Gordon <rgordon@rsgc.on.ca>
2017-04-02 09:26:07 -0400

    All done the Sequence exercise

--- Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -32,19 +32,31 @@ canvas.drawShapesWithBorders = false
 
 // Draw the red circle
 canvas.fillColor = Color.red
-canvas.drawEllipse(centreX: 125, centreY: 150, width: 175, height: 175)
+canvas.drawEllipse(centreX: 100, centreY: 150, width: 175, height: 175)
 
 // Draw the yellow rounded rectangle
 //
 // NOTE: Used this document to find accurate colour...
 // http://russellgordon.ca/rsgc/2016-17/ics2o/HSB_Colour_Model_-_Summary_-_Swift_3.pdf
+// ... and this app ...
+// http://russellgordon.ca/rsgc/ics/c3d.zip
 //
 canvas.fillColor = Color(hue: 43, saturation: 100, brightness: 94, alpha: 100)
-canvas.drawRoundedRectangle(centreX: 350, centreY: 200, width: 125, height: 175, borderWidth: 1, xRadius: 25, yRadius: 25)
+canvas.drawRoundedRectangle(centreX: 325, centreY: 200, width: 125, height: 175, borderWidth: 1, xRadius: 25, yRadius: 25)
 
 // Draw the blue rectangle
 canvas.fillColor = Color(hue: 222, saturation: 100, brightness: 100, alpha: 75)
-canvas.drawRectangle(centreX: 250, centreY: 190, width: 140, height: 300)
+canvas.drawRectangle(centreX: 225, centreY: 190, width: 140, height: 300)
+
+// Define the co-ordinates for the triangle
+var points : [NSPoint] = [] // Create an empty list, or array, of points
+points.append(NSPoint(x: 340, y: 325))
+points.append(NSPoint(x: 490, y: 325))
+points.append(NSPoint(x: 415, y: 100))
+
+// Draw the triangle using the drawCustomShape method of the canvas object
+canvas.fillColor = Color(hue: 88, saturation: 100, brightness: 75, alpha: 75)
+canvas.drawCustomShape(with: points)
 
 /*:
  ## Template code
